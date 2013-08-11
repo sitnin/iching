a117680c44d9b2722063bc006a799495122948b1
Gregory Sitnin <greg@sitnin.com>
2013-08-11 16:56:15 +0400

    Slightly refactored node.js test

--- tests/test.js
+++ tests/test.js
@@ -9,15 +9,19 @@
 var util = require("util");
 var iching = require('../build/Release/iching');
 
-function test(year, month, day, hour) {
+function test(year, month, day, hour, reference) {
     var h = iching.getHexagrams(year, month, day, hour);
-    console.log(util.format("res: %d - %d - %d - %d", h.birth, h.core, h.end, h.mline));
+    var s = util.format("%d - %d - %d - %d", h.birth, h.core, h.end, h.mline);
+    var testRest = s === reference;
+    console.log("\nRES:", s, "\nREF:", reference, testRest ? "OK" : "FAIL");
+    return testRest ? 0 : 1;
 }
 
-console.log("I-Ching Node.JS Library binding test");
+console.log("iChing Node.JS Library binding test");
 
-test(1973, 12, 3, 0);
-console.log("ref: 51 - 39 - 54 - 2\n");
+var errCount = 0;
+errCount += test(1973, 12, 3, 0, "51 - 39 - 54 - 2");
+errCount += test(1980, 11, 24, 1, "23 - 2 - 2 - 6");
 
-test(1980, 11, 24, 1);
-console.log("ref: 23 - 2 - 2 - 6\n");
+console.log("\nErrors:", errCount);
+process.exit(errCount);
